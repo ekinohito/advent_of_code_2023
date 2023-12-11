a34d8dcd12c52d5423c384b9aac3831e43e61d97
ekinohito <zhenyagur9@gmail.com>
2023-12-11 13:13:46 +0300

    total mess but works

--- days/day_10/src/part_2.rs
+++ days/day_10/src/part_2.rs
@@ -45,8 +45,27 @@ pub fn solution(input: &str) -> u64 {
     let mut left = adjacents.next().expect("No adjacent pipes");
     let mut right = adjacents.next().expect("Only one adjacent pipe");
     assert_eq!(adjacents.next(), None, "Too many adjacent pipes");
-    blocks[start_i][start_j] = Pipe(left.2.opposite(), right.2.opposite());
+    blocks[start_i][start_j] = MainPipe(left.2.opposite(), right.2.opposite());
     dbg!(&blocks[start_i][start_j]);
+    blocks.iter().for_each(|row| {
+        row.iter().for_each(|b| {
+            print!(
+                "{}",
+                match *b {
+                    Ground => '.',
+                    Block::TB => '┃',
+                    Block::RL => '━',
+                    Block::TR => '┗',
+                    Block::RB => '┏',
+                    Block::BL => '┓',
+                    Block::LT => '┛',
+                    Start => 'x',
+                    _ => 'U',
+                }
+            )
+        });
+        println!()
+    });
 
     while left.0 != right.0 || left.1 != right.1 {
         for (i, j, side) in [&mut left, &mut right] {
@@ -61,6 +80,7 @@ pub fn solution(input: &str) -> u64 {
             *side = next_side.opposite();
         }
     }
+    blocks[left.0][left.1] = if let Pipe(s1, s2) = blocks[left.0][left.1] {MainPipe(s1, s2) } else {panic!()};
 
     blocks.iter_mut().for_each(|row| {
         row.iter_mut().for_each(|block| {
@@ -72,6 +92,25 @@ pub fn solution(input: &str) -> u64 {
         })
     });
 
+    blocks.iter().for_each(|row| {
+        row.iter().for_each(|b| {
+            print!(
+                "{}",
+                match *b {
+                    Ground => '.',
+                    Block::TB => '┃',
+                    Block::RL => '━',
+                    Block::TR => '┗',
+                    Block::RB => '┏',
+                    Block::BL => '┓',
+                    Block::LT => '┛',
+                    Start => 'x',
+                    _ => 'U',
+                }
+            )
+        });
+        println!()
+    });
 
     let mut result = 0;
     for row in &mut blocks {
@@ -84,13 +123,17 @@ pub fn solution(input: &str) -> u64 {
                         result += 1;
                         *block = Start;
                     }
-                },
+                }
                 Pipe(Top, Bottom) => {
                     within_loop = !within_loop;
                 }
-                Pipe(Right, Left) => {},
+                Pipe(Right, Left) => {}
                 Pipe(side_1, side_2) => {
-                    let direction = if *side_1 == Top || *side_2 == Top {Top} else {Bottom};
+                    let direction = if *side_1 == Top || *side_2 == Top {
+                        Top
+                    } else {
+                        Bottom
+                    };
                     if let Some(start_direction) = start {
                         if start_direction != direction {
                             within_loop = !within_loop;
@@ -99,22 +142,49 @@ pub fn solution(input: &str) -> u64 {
                     } else {
                         start = Some(direction);
                     }
-                },
-                _ => unreachable!()
+                }
+                _ => unreachable!(),
             }
         }
     }
-    blocks.iter().for_each(|row| {row.iter().for_each(|b| print!("{}", match *b {
-        Ground => '.',
-        Block::TB => '┃',
-        Block::RL => '━',
-        Block::TR => '┗',
-        Block::RB => '┏',
-        Block::BL => '┓',
-        Block::LT => '┛',
-        Start => 'x',
-        _ => 'U',
-    })); println!()});
+    blocks.iter().for_each(|row| {
+        row.iter().for_each(|b| {
+            print!(
+                "{}",
+                match *b {
+                    Ground => '.',
+                    Block::TB => '┃',
+                    Block::RL => '━',
+                    Block::TR => '┗',
+                    Block::RB => '┏',
+                    Block::BL => '┓',
+                    Block::LT => '┛',
+                    Start => 'x',
+                    _ => 'U',
+                }
+            )
+        });
+        println!()
+    });
 
     result
+}
+
+#[test]
+fn test_solution() {
+    assert_eq!(
+        solution(
+            r#"FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L"#
+        ),
+        10
+    );
 }
